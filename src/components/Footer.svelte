4500277b5eef526cd831279c3550250fa9ca7d63
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2025-03-24 11:28:23 +0530

    clean footer links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,40 +2,40 @@
     <div class="px-4 flex flex-col sm:flex-row w-full justify-between items-center">
         <div class="flex-col justify-center items-center w-full sm:w-[50%]">
             <div class="px-6 flex items-center justify-center sm:justify-start overflow-hidden xs:justify-center">
-                <a href="https://www.acmvit.in/" target="_blank">
-                    <img alt="ACM VTI" loading="lazy" width="63" height="63" decoding="async" data-nimg="1" src="/favicon.png" style="color: transparent;">
+                <a href="/" >
+                    <img alt="" width="63" height="63" src="/favicon.png" style="color: transparent;">
                 </a>
             </div>
             <div class="flex text-[0.8rem] space-x-4 mt-2 sm:mt-4 xs:justify-center sm:justify-start">
-                <a href="https://www.acmvit.in/" target="_blank" rel="noopener noreferrer">
+                <a href="/">
                     <h2 class="text-[#FCF3C5] hover:text-white">Download Now</h2>
                 </a>
             </div>
             <div class="flex text-[0.8rem] space-x-4 mt-2 sm:mt-4 xs:justify-center sm:justify-start">
-                <a href="https://www.acmvit.in/" target="_blank" rel="noopener noreferrer">
+                <a href="/">
                     <h2 class="text-[#FCF3C5] hover:text-white">About</h2>
                 </a>
-                <a href="https://gravitas.vit.ac.in/events/ea3eb2e8-7036-4265-9c9d-ecb8866d176b" target="_blank" rel="noopener noreferrer">
+                <a href="/">
                     <h2 class="text-[#FCF3C5] hover:text-white">Pricing</h2>
                 </a>
-                <a href="https://localhost.acmvit.in/landing" target="_blank" rel="noopener noreferrer">
+                <a href="/">
                     <h2 class="text-[#FCF3C5] hover:text-white">Careers</h2>
                 </a>
-                <a href="mailto:outreach.acmvit@gmail.com" target="_blank" rel="noopener noreferrer"><h2 class="text-[#FCF3C5] hover:text-white">Help</h2>
+                <a href="/"><h2 class="text-[#FCF3C5] hover:text-white">Help</h2>
                 </a>
-                <a href="https://www.youtube.com/watch?v=mEaJHat-2dA" target="_blank" rel="noopener noreferrer">
+                <a href="/">
                     <h2 class="text-[#FCF3C5] hover:text-white">Privacy Policy</h2>
                 </a>
             </div>
             <div class="flex text-[0.8rem] space-x-4 mt-2 sm:mt-4 xs:justify-center sm:justify-start">
-                <h2 class="text-[#FCF3C5]">© 2024 ACM VIT. All rights reserved</h2>
+                <h2 class="text-[#FCF3C5]">VarAInce BMSCE</h2>
             </div>
         </div>
         <div class="flex space-x-4 mt-4 sm:mt-0">
-            <a href="https://www.linkedin.com/company/acmvit/mycompany/" class="text-gray-400 hover:text-white" target="_blank"><img alt="LinkedIn" loading="lazy" width="32" height="32" decoding="async" data-nimg="1" src="/favicon.png" style="color: transparent;"></a>
-            <a href="https://www.instagram.com/acmvit" class="text-gray-400 hover:text-white" target="_blank"><img alt="Instagram" loading="lazy" width="32" height="32" decoding="async" data-nimg="1" src="/favicon.png" style="color: transparent;"></a>
-            <a href="https://x.com/ACM_VIT?t=BW_ApfkIuJnuCpTSJqk9pg&amp;s=08" class="text-gray-400 hover:text-white" target="_blank"><img alt="x" loading="lazy" width="32" height="32" decoding="async" data-nimg="1" src="/favicon.png" style="color: transparent;"></a>
-            <a href="https://youtube.com/@acm_vit?feature=shared" class="text-gray-400 hover:text-white" target="_blank"><img alt="YouTube" loading="lazy" width="32" height="32" decoding="async" data-nimg="1" src="/favicon.png" style="color: transparent;"></a>
+            <a href="/" class="text-gray-400 hover:text-white"><img alt="LinkedIn" width="32" height="32" src="/favicon.png" style="color: transparent;"></a>
+            <a href="/" class="text-gray-400 hover:text-white"><img alt="Instagram" width="32" height="32" src="/favicon.png" style="color: transparent;"></a>
+            <a href="/" class="text-gray-400 hover:text-white"><img alt="x" width="32" height="32" src="/favicon.png" style="color: transparent;"></a>
+            <a href="/" class="text-gray-400 hover:text-white"><img alt="YouTube" width="32" height="32" src="/favicon.png" style="color: transparent;"></a>
         </div>
     </div>
 </footer>
