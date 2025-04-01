5b82cae146a875223f54605e60e73a2db71af40f
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2025-04-01 12:23:51 +0530

    fix social icons aligment

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,7 +7,7 @@
     <div class="flex items-center text-center">
         <h2 class="text-white font-bold text-4xl">VarAInce BMSCE</h2>
     </div>
-    <div class="flex mt-4 items-center">
+    <div class="flex my-auto">
         <a href="https://www.linkedin.com/company/varaince-club/" class="text-gray-400 mx-2 hover:text-white"><img alt="LinkedIn" width="32" height="32" src="/linkedin.png" style="color: transparent;"></a>
         <a href="https://www.instagram.com/varaince_bmsce/" class="text-gray-400 mx-2 hover:text-white"><img alt="Instagram" width="32" height="32" src="/instagram.png" style="color: transparent;"></a>
     </div>
