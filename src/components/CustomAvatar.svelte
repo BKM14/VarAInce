2fefb5870cef8e8bba414e4aab81ad359e5a6ed3
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2025-04-01 16:46:51 +0530

    fix gallery breaking on mobile

---
+++ src/components/CustomAvatar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Avatar } from "flowbite-svelte";
+    
+    export let profile: Profile;
+</script>
+
+<script context="module" lang="ts">
+    export interface Profile {
+        image: string;
+        imageAlt: string;
+        name: string;
+        post: string;
+    }
+</script>
+
+<div class="text-center">
+    <Avatar src={profile.image} alt={profile.imageAlt} class="w-23 h-22 lg:w-38 lg:h-36 mx-auto"/>
+    <div class="text-white font-bold">{profile.name}</div>
+    <div class="text-white font-light">{profile.post}</div>
+</div>
--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -1,64 +1,32 @@
 <script lang="ts">
-    import { Avatar, Gallery } from "flowbite-svelte";
+    import CustomAvatar from "./CustomAvatar.svelte";
+    import type { Profile } from "./CustomAvatar.svelte";
     
-    const images1 = [
-    { alt: 'erbology', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg' },
-    { alt: 'shoes', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg' },
-    { alt: 'small bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg' }
-  ];
-  const images2 = [
-    { alt: 'plants', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg' },
-    { alt: 'watch', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg' },
-    { alt: 'shoe', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg' }
-  ];
-  const images3 = [
-    { alt: 'cream', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg' },
-    { alt: 'small bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg' },
-    { alt: 'lamp', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg' }
-  ];
-  const images4 = [
-    { alt: 'toiletbag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg' },
-    { alt: 'playstation', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg' },
-    { alt: 'bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg' }
+    const profiles: Profile[] = [
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg', imageAlt: 'erbology', name: 'Alex Carter', post: 'Software Lead' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg', imageAlt: 'shoes', name: 'Jordan Smith', post: 'Hardware Specialist' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg', imageAlt: 'small bag', name: 'Samantha Lee', post: 'Design Head' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg', imageAlt: 'plants', name: 'Rahul Mehta', post: 'AI Engineer' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg', imageAlt: 'watch', name: 'Elena Garcia', post: 'Embedded Systems Expert' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg', imageAlt: 'shoe', name: 'Daniel Brown', post: 'Cybersecurity Analyst' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg', imageAlt: 'cream', name: 'Priya Sharma', post: 'Machine Learning Lead' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg', imageAlt: 'small bag', name: 'Lucas Johnson', post: 'Full Stack Developer' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg', imageAlt: 'lamp', name: 'Sophia Williams', post: 'Robotics Engineer' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg', imageAlt: 'toiletbag', name: 'Ethan Martinez', post: 'Game Developer' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg', imageAlt: 'playstation', name: 'Olivia Kim', post: 'Cloud Architect' },
+    { image: 'https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg', imageAlt: 'bag', name: 'Michael Anderson', post: 'Blockchain Specialist' }
   ];
 
-  export let eventPhotos: Boolean = false;
   export let galleryTitle: String = "";
 </script>
 
-<div class="md:w-3/5 mx-auto p-10">
+<div class="w-full md:w-3/5 mx-auto p-10">
   <div>
-    <h2 class="text-white text-4xl font-bold md:text-6xl text-center md:m-10 m-4">{galleryTitle}</h2>
-    {#if eventPhotos}
-    <Gallery class="gap-4 grid-cols-3 md:grid-cols-4" items={[]}>
-        <Gallery items={images1} />
-        <Gallery items={images2} />
-        <Gallery items={images3} />
-        <Gallery items={images4} />
-      </Gallery>
-      {:else}
-      <Gallery class={`gap-4 grid-cols-3 md:grid-cols-4`} items={[]}>
-        <Gallery items={images1} let:item>
-            <Avatar src={item.src} alt={item.alt} class="w-23 h-18 lg:w-48 lg:h-38"/>
-            <div class="text-white font-bold text-center">Name</div>
-            <div class="text-white font-light text-center">Post</div>
-        </Gallery>
-        <Gallery items={images2} let:item>
-            <Avatar src={item.src} alt={item.alt} class="w-23 h-18 lg:w-48 lg:h-38"/>
-            <div class="text-white font-bold text-center">Name</div>
-            <div class="text-white font-light text-center">Post</div>
-        </Gallery>
-        <Gallery items={images3} let:item>
-            <Avatar src={item.src} alt={item.alt} class="w-23 h-18 lg:w-48 lg:h-38"/>
-            <div class="text-white font-bold text-center">Name</div>
-            <div class="text-white font-light text-center">Post</div>
-        </Gallery>
-        <Gallery items={images4} let:item>
-            <Avatar src={item.src} alt={item.alt} class="w-23 h-18 lg:w-48 lg:h-38"/>
-            <div class="text-white font-bold text-center">Name</div>
-            <div class="text-white font-light text-center">Post</div>
-        </Gallery>
-      </Gallery>
-    {/if}
+    <h2 class="text-white text-xl font-bold md:text-3xl text-center md:m-10 mb-4">{galleryTitle}</h2>
+    <div class="grid grid-cols-3 lg:grid-cols-4 gap-4">
+      {#each profiles as profile}
+      <CustomAvatar profile={profile}/>
+      {/each}
+    </div>
   </div>
 </div>
